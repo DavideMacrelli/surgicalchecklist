2dae4f448da9524ec7fb04c6071d1639c58582e9
DavideMacrelli <mcdavide92@gmail.com>
2016-12-07 09:01:41 +0100

    Non-conformity Sheet render logic
    Template for NC-sheet rendering is fixed.
    Checklist data: Phase 1 fixed, phase 2 compiled

--- app/checklist-view/checklist-view.js
+++ app/checklist-view/checklist-view.js
@@ -10,7 +10,7 @@ angular.module('checklistApp.checklistView', [])
 }])
 
 .controller('checklistController',['$scope', '$log', 'listState', '$http', 'errorPage', function($scope, $log, listState, $http, signalError) {
-
+    //TODO: fare un servizio che recupera risorse, 
     var checklistURL = "content/checklist.json";
     var nonConformitySheetURL = "content/non-conformity-sheet.json";
     $scope.checklist = [];
