ba5b7784e052a740b88aecb5f5b457ee930cfc60
DavideMacrelli <mcdavide92@gmail.com>
2016-12-16 13:42:07 +0100

    Checklist template name change

--- app/components/checklist/checklist-directive.js
+++ app/components/checklist/checklist-directive.js
@@ -17,7 +17,7 @@ angular.module('checklistApp.checklistDirective',[])
             },
 
             //il file html allegato all'interno dell'elemento con la direttiva
-            templateUrl: 'components/checklist/checklist-776.html',
+            templateUrl: 'components/checklist/checklist-template.html',
 
             //TODO: isolare lo scope, in questo momento la direttiva è dipendente dallo scope della 'checklist-view'
 
