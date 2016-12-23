661e92b52fa3c8c129031729f91b2be2af8bf05b
DavideMacrelli <mcdavide92@gmail.com>
2016-12-23 19:55:41 +0100

    More Css styles

--- app/checklist-view/checklist-view.js
+++ app/checklist-view/checklist-view.js
@@ -26,35 +26,80 @@ angular.module('checklistApp.checklistView', [])
             listState.nextStep();
         },
 
-        prevStep: function () {
-            listState.prevStep();
-        },
+        stepControls: [
+            {
+                label: "Passo precedente",
+                behaviour: function () {
+                    listState.prevStep();
+                },
+            },
 
-        signalNonConformity: function (phaseNum, stepNum) {
-            $log.log("Non conformità all passo: " + stepNum + " fase: " + phaseNum);
-            // controllo che esista un passo m alla fase n nella scheda delle non conformità
-            if ($scope.nonConformitySheet[phaseNum].steps[stepNum].legend != "Non Disponibile") {
-                $log.log("C'è!: " + $scope.nonConformitySheet[phaseNum].steps[stepNum].legend);
-                $scope.nonConformityView = true;
+            {
+                label: "N-C Mod 776/b",
+                behaviour: function (phaseNum, stepNum) {
+                    $log.log("Non conformità all passo: " + stepNum + " fase: " + phaseNum);
+                    // controllo che esista un passo m alla fase n nella scheda delle non conformità
+                    if ($scope.nonConformitySheet[phaseNum].steps[stepNum].legend != "Non Disponibile") {
+                        $log.log("C'è!: " + $scope.nonConformitySheet[phaseNum].steps[stepNum].legend);
+                        $scope.nonConformityView = true;
+                    }
+
+                },
             }
-        },
 
-        nextPhase: function () {
-            listState.nextPhase();
-        }
+        ],
+
+        endPhaseControls: [
+            {
+                label: "Torna al passo precedente",
+                behaviour: function () {
+                    listState.prevStep();
+                },
+            },
+
+            {
+                label: "Procedi alla prossima fase",
+                behaviour: function () {
+                    listState.nextPhase();
+                }
+            }
+
+        ],
+
+        endListControls: [
+            {
+                label: "Torna al passo precedente",
+                behaviour: function () {
+                    listState.prevStep();
+                },
+            },
+
+            {
+                label: "Completa Checklist",
+                behaviour: function () {
+                    $log.log("Checklist Completata");
+                },
+            },
+        ]
     };
 
     $scope.nonConformityControls = {
-        nextStep: function () {
-        },
+        nextStep: function () { },
 
-        prevStep: function () {
+        stepControls: [
+            {
+                label: "Segnala non-conformità",
+                behaviour: function (phaseNum, stepNum) {
+                        $scope.nonConformityView = false;
 
-        },
+                },
+            }
 
-        signalNonConformity: function () {
-            $scope.nonConformityView = false;
-        }
+        ],
+
+        endPhaseControls: [],
+
+        endListControls: []
     };
 
     //ogni volta che il controller viene eliminato, fa il listen sull'evento $destroy
